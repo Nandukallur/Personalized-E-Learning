7ae1d5b169a557faeb8f892a798277de055c5c4d
Nandukallur <135836672+Nandukallur@users.noreply.github.com>
2024-02-20 22:14:14 +0530

    Update urls.py

--- Compiler/urls.py
+++ Compiler/urls.py
@@ -7,4 +7,10 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name="indexpage"),
     path('index/runcode/', views.runcode, name="runcode"),
+    path('coding_answers/', views.Coding_section, name="codinganswers"),
+    path('htmlcoding/', views.html_questions, name="htmlcoding"),
+    path('phpcoding/', views.php_questions, name="phpcoding"),
+    path('htmlanswers/', views.html_answers, name="htmlanswers"),
+    path('phpanswers/', views.php_answers, name="phpanswers")
 ]
+
